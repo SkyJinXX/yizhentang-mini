670aa5c139fb09763f30df5645bba290f78742ce
SkyJin <skyjin@outlook.com>
2019-05-17 17:17:32 +0800

    妈妈课堂完成

---
+++ pages/class/class.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+
+  },
+  submit: function () {
+    wx.showToast({
+      title: '报名成功',
+      icon: 'success',
+      duration: 2000
+    })
+  }
+})
--- pages/consult/consult.js
+++ pages/consult/consult.js
@@ -1,8 +1,27 @@
 Page({
   data: {
-
+    ifShowFull: false
   },
   onLoad: function (options) {
 
   },
+  toggleFull: function () {
+    this.setData({
+      ifShowFull: !this.data.ifShowFull
+    })
+  },
+  submit: function () {
+    wx.showToast({
+      title: '提交成功',
+      icon: 'success',
+      duration: 2000
+    })
+  },
+  choose: function () {
+    wx.showToast({
+      title: '暂无其他科室选择',
+      icon: 'none',
+      duration: 2000
+    })
+  }
 })
